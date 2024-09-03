51fc9067390352b99db9f586d22265af747a30c1
Tarekjalali <jlali.tare9@gmail.com>
2024-09-03 14:59:28 +0100

    ws state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      tasks:[
+        // {title:"Task1", isDone: false, id:Math.random()},
+        // {title:"Task2", isDone: true, id:Math.random()}
+
+      ],
+      livetext:""
+    }
+    
+    
+  }
+
+  addTask=()=>this.setState({tasks:[...this.state.tasks,{title:this.state.livetext, isDone:false, id:Math.random()}],livetext:""})
+  switchDone=(a)=>this.setState({tasks:this.state.tasks.map((el,i,t)=>el.id===a?{...el,isDone:!el.isDone}:el)})
+  deleteTask=(a)=>this.setState({tasks:this.state.tasks.filter((el,i,t)=>el.id!==a)})
+ 
+
+  render(){
+    return(
+      <div>
+      <h1>ToDo List</h1>
+      {this.state.tasks.map((el,i,t)=><div>
+        <h3 className={el.isDone && "cross"}>{el.title}</h3>
+        {/* <h4>{el.isDone? "TRUE":"FALSE"}</h4> */}
+        <button onClick={()=>this.switchDone(el.id)}>Done</button>
+        <button onClick={()=>this.deleteTask(el.id)}>delete</button>
+        </div>)}
+
+        <br/>
+        <br/>
+        <input type='text' value={this.state.livetext} onChange={(e)=>this.setState({livetext:e.target.value})}></input>
+        <button onClick={this.addTask}>add</button>
+      </div>
+    )
+  }
 }
+ 
+
+
 
 export default App;
